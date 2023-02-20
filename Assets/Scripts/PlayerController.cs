0fb977f7ee43439c00be6bfcb4791a0f05ad13e6
fortimindkal <bintangmuhammadhaikal@gmail.com>
2023-02-20 18:34:05 +0700

    New Projects + Player Controller
    Added new projects and Player Controller Scripts.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;        // Kecepatan gerakan karakter
+
+    private Vector2 moveDir;      // Arah gerakan karakter
+    private Vector2 mousePos;   // Posisi Mouse
+
+    public Rigidbody2D rb;
+    public Camera cam;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        // Mendapatkan posisi kursor mouse
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // Mendapatkan input gerakan karakter
+        moveDir.x = Input.GetAxisRaw("Horizontal");
+        moveDir.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // Menggerakkan karakter sesuai arah gerakan dan kecepatan
+        rb.MovePosition(rb.position + moveDir * moveSpeed * Time.fixedDeltaTime);
+
+        // Mengarahkan senjata ke arah kursor mouse
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+
+}
